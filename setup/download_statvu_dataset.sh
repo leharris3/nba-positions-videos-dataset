481d7b1883a1dfb9038da55522cf36563f6b4936
Levi Harris <69766046+leharris3@users.noreply.github.com>
2023-08-27 14:52:32 -0400

    working setup scripts

--- setup/download_statvu_dataset.sh
+++ setup/download_statvu_dataset.sh
@@ -6,5 +6,5 @@ DRIVE_FOLDER_LINK="https://drive.google.com/drive/folders/1OkPb160SAhD5S5RXjYN1e
 gdown --folder "$DRIVE_FOLDER_LINK" -O "$DESTINATION_DIR"
 
 echo "StatVU dataset downloaded."
-7z x "$DESTINATION_FILE" -o "$DESTINATION_DIR"
+7z x "$DESTINATION_FILE" "-o$DESTINATION_DIR"
 rm "$DESTINATION_FILE"
