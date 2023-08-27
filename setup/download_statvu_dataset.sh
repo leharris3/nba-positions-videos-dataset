49d706aab44ad76cbbe2ee7d7810553a638d63d8
Levi Harris <69766046+leharris3@users.noreply.github.com>
2023-08-27 10:18:54 -0400

    some setup tweaks

--- setup/download_statvu_dataset.sh
+++ setup/download_statvu_dataset.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
 DESTINATION_DIR="statvu-raw-data"
-DRIVE_FOLDER_LINK="https://drive.google.com/drive/folders/11Qr3WjjjvuyXema2sRECvh63RsK9Dx7l?usp=drive_link"
+DRIVE_FOLDER_LINK="https://drive.google.com/drive/folders/1OkPb160SAhD5S5RXjYN1enHAV82tJlzu"
 gdown "$DRIVE_FOLDER_LINK" -O "$DESTINATION_DIR"
 
 echo "StatVU dataset downloaded."
+
+# Search for .7z file in the destination folder
+7Z_FILE=$(find "$DESTINATION_DIR" -name "*.7z" -exec basename {} \;)
+
+if [ -n "$7Z_FILE" ]; then
+    # Unzip the .7z file
+    7z x "$DESTINATION_DIR/$7Z_FILE" -o"$DESTINATION_DIR"
+
+    if [ $? -eq 0 ]; then
+        echo "File extracted successfully."
+        # Delete the .7z file
+        rm "$DESTINATION_DIR/$7Z_FILE"
+        echo "File deleted."
+    else
+        echo "Extraction failed."
+    fi
+else
+    echo "No .7z file found in the destination folder."
+fi
--- setup/download_videos.sh
+++ setup/download_videos.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DESTINATION_DIR="unprocess-videos"
-DRIVE_FOLDER_LINK="https://drive.google.com/drive/folders/1ODZDZsWWsXpTmJnxU016spv6KJGQQkF0?usp=drive_link"
+DRIVE_FOLDER_LINK="https://drive.google.com/drive/folders/1ODZDZsWWsXpTmJnxU016spv6KJGQQkF0"
 gdown "$DRIVE_FOLDER_LINK" -O "$DESTINATION_DIR"
 
 echo "Estimated size of download: $ESTIMATED_SIZE"
