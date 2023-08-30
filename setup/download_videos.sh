2bb4d1cffe6e8c20df7d2d4638b6bf814cac8f1e
Levi Harris <69766046+leharris3@users.noreply.github.com>
2023-08-30 12:56:11 -0400

    clean slate

--- setup/download_videos.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-DESTINATION_DIR="unprocess-videos"
-DRIVE_FOLDER_LINK="https://drive.google.com/drive/folders/1ODZDZsWWsXpTmJnxU016spv6KJGQQkF0"
-gdown --folder "$DRIVE_FOLDER_LINK" -O "$DESTINATION_DIR"
