6954179025dcaa745563d1d17a69bd0593f6a592
Levi Harris <69766046+leharris3@users.noreply.github.com>
2023-09-02 10:43:11 -0400

    misc

---
+++ pipeline.py
@@ -0,0 +1,7 @@
+from game import Game
+
+
+class Pipeline:
+
+    def process_game(self, Game):
+        pass
--- video.py
+++ video.py
@@ -2,6 +2,7 @@
 # living path to video file
 #
 
+import cv2
 import os
 
 
@@ -12,10 +13,32 @@ class Video:
         assert os.path.exists(path), f"Error: bad path to video."
         self.path = path
 
-    def get_path(self):
+    def get_path(self) -> str:
         return self.path
 
-    def move(to: str) -> None:
-        assert os.path.exists(to), f"Error: bad path {to}."
+    def set_path(self, path: str) -> None:
+        assert os.path.exists(path), f"Error: path not found."
+        self.path = path
 
+    def get_video_attributes(self):
+        """Getter method for basic video attributes."""
+
+        cap = cv2.VideoCapture(self.get_path())
+        if not cap.isOpened():
+            raise Exception(
+                f"Error: Unable to open video at path: {self.get_path()}.")
+        frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        cap.release()
+        video_attributes = {
+            "width": frame_width,
+            "height": frame_height,
+            "fps": fps,
+            "frame_count": frame_count
+        }
+        return video_attributes
+
+    def move(to: str) -> None:
         pass
