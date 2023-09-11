c769ee4d39b475f1e17abf34dc300510c3f7ca6e
Levi Harris <69766046+leharris3@users.noreply.github.com>
2023-09-11 15:50:37 -0400

    removed some dirs

--- pipeline.py
+++ pipeline.py
@@ -5,6 +5,9 @@ class Pipeline:
 
     def process_game(self, game: Game):
 
+        # 1. find roi
+        # 2. extract timestamps from roi
+        # 3. post process results
         # roi = find_roi(game)
         # timestamps = extract_timestamps(game=game, roi=roi)
         # json.save(out_path, timestamps)
