7aac5a2997a4ad4d715f4b639b827794ab1a56c4
Levi Harris <69766046+leharris3@users.noreply.github.com>
2023-10-04 07:27:59 -0400

    some updates

--- pipeline.py
+++ pipeline.py
@@ -3,6 +3,7 @@ import torch
 import json
 import os
 import re
+import time
 import numpy as np
 
 from tqdm import tqdm
@@ -12,8 +13,6 @@ from typing import List
 from viz import visualize_timestamps
 from utilities.models import Models
 from utilities.constants import *
-
-MODELS = Models()
 
 
 def process_dir(dir_path: str, data_out_path: str, viz_out_path=None) -> None:
@@ -30,14 +29,26 @@ def process_dir(dir_path: str, data_out_path: str, viz_out_path=None) -> None:
             viz_out_path) is str, "Error: path links must be of type str."
         os.makedirs(viz_out_path, exist_ok=True)
 
+    valid_formats = ['avi', 'mp4']
     vids = os.listdir(dir_path)
     for vid in vids:
+        extension = vid.split(".")[1]
+        if extension not in valid_formats:
+            vids.remove(vid)
+
+    for vid in vids:
         video_path = os.path.join(dir_path, vid)
-        save_path = os.path.join(data_out_path, vid.replace(".mp4", ".json"))
-        viz_path = os.path.join(viz_out_path, vid.replace(".mp4", "_viz.avi"))
-        extract_timestamps_from_video(video_path, save_path)
+        data_path = os.path.join(data_out_path, vid.replace(
+            ".mp4", ".json").replace(".avi", ".json"))
+        start_time = time.time()  # for benchmarking purposes
+        extract_timestamps_from_video(video_path, data_path)
         if viz_out_path is not None:
-            visualize_timestamps(video_path, save_path, viz_path)
+            viz_path = os.path.join(
+                viz_out_path, vid.replace(".mp4", "_viz.avi"))
+            visualize_timestamps(video_path, data_path, viz_path)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"{vid} processed in {elapsed_time:.2f} seconds")
 
 
 def extract_timestamps_from_video(video_path: str, save_path: str) -> None:
@@ -46,13 +57,15 @@ def extract_timestamps_from_video(video_path: str, save_path: str) -> None:
     saves a json with extracted timestamp info to save path.
     """
 
-    assert os.path.isfile(video_path)
-    assert not os.path.isfile(
-        save_path), f"Warning, overwriting exisiting file at {save_path}"
-    assert video_path[-4:
-                      ] == '.mp4', f"Bad video format: {video_path}, must be an mp4."
+    assert os.path.exists(video_path)
+    # TODO: FIX!
+    # assert os.path.exists(
+    #     save_path), f"Warning, overwriting exisiting file at {save_path}"
+    # assert video_path[-4:
+    #                   ] == '.mp4', f"Bad video format: {video_path}, must be an mp4."
 
     print(f"Extracting timestamps for video at {video_path}")
+    tr_x1, tr_y1, tr_x2, tr_y2 = None, None, None, None
     time_remaining_roi = extract_roi_from_video(video_path)
     if time_remaining_roi is not None:
         tr_x1, tr_y1, tr_x2, tr_y2 = time_remaining_roi
@@ -60,6 +73,7 @@ def extract_timestamps_from_video(video_path: str, save_path: str) -> None:
     frames_cnt = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     timestamps = {}
     quarter = video_path[-5]  # period_x.mp4
+    step = 5
 
     for frame_index in tqdm(range(frames_cnt)):
         ret, frame = cap.read()
@@ -67,13 +81,15 @@ def extract_timestamps_from_video(video_path: str, save_path: str) -> None:
             break
         time_remaining_img = None
         time_remaining = None
-        if time_remaining_roi is not None:
-            time_remaining_img = frame[tr_y1 -
-                                       PAD: tr_y2 + 2 * PAD, tr_x1 - PAD: tr_x2 + 2 * PAD]
-        if not time_remaining_img is None:
-            time_remaining = extract_time_remaining_from_image(
-                Image.fromarray(time_remaining_img))
-            time_remaining = convert_time_to_float(time_remaining)
+        if frame_index % step == 0:
+            if time_remaining_roi is not None:
+                assert tr_x1 and tr_y1 and tr_x2 and tr_y2
+                time_remaining_img = frame[tr_y1 -
+                                           PAD: tr_y2 + 2 * PAD, tr_x1 - PAD: tr_x2 + 2 * PAD]
+            if time_remaining_img is not None:
+                time_remaining = extract_time_remaining_from_image(
+                    Image.fromarray(time_remaining_img))
+                time_remaining = convert_time_to_float(time_remaining)
         timestamps[str(frame_index)] = {
             "quarter": quarter,
             "time_remaining": time_remaining
@@ -95,11 +111,9 @@ def extract_roi_from_video(video_path: str):
 
     assert os.path.isfile(
         video_path), f"Error: bad path to video {video_path}."
-    assert video_path[-4:] == '.mp4'
+    # assert video_path[-4:] == '.mp4'
 
     print(f"Finding time-remaining ROI for video at {video_path}")
-    if not MODELS.models_loaded:
-        MODELS.load()
     cap = cv2.VideoCapture(video_path)
     frames_cnt = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     time_remaining_roi = None
@@ -107,14 +121,14 @@ def extract_roi_from_video(video_path: str):
     # TODO: skip through vid at one second intervals
     highest_conf = 0.0
     best_roi = None
-    interval = 30
+    step = 30
 
     for i in tqdm(range(frames_cnt)):
         ret, frame = cap.read()
         if not ret:
             break
-        if (i % interval == 0):
-            results = MODELS.yolo(frame, verbose=False)
+        if (i % step == 0):
+            results = Models.yolo(frame, verbose=False)
             classes, conf, boxes = results[0].boxes.cls, results[0].boxes.conf, results[0].boxes.xyxy
             classes_conf = torch.stack((classes, conf), dim=1)
             predictions = torch.cat((classes_conf, boxes), dim=1)
@@ -137,14 +151,14 @@ def extract_roi_from_video(video_path: str):
     return best_roi
 
 
-def find_time_remaining_from_results(results: [str]):
+def find_time_remaining_from_results(results: List[str]):
     """
     Matches any string showing a valid time remaining of 20 minutes or less
     assumes brodcasts use MM:SS for times > 1 minute, and SS.S for times < 1 minute
     """
     if results is None:
         return None
-    time_remaining_regex = "(20:00)|(0[0-9]?:[0-9][0-9](\.[0-9])?)|([1-9]:[0-5][0-9])|(1[0-9]:[0-5][0-9](\.[0-9])?)|([0-9]\.[0-9])|([1-5][0-9]\.[0-9])"
+    time_remaining_regex = r"(20:00)|(0[0-9]?:[0-9][0-9](\.[0-9])?)|([1-9]:[0-5][0-9])|(1[0-9]:[0-5][0-9](\.[0-9])?)|([0-9]\.[0-9])|([1-5][0-9]\.[0-9])"
     for result in results:
         result = result.replace(" ", "")
         match = re.match(time_remaining_regex, result)
@@ -153,19 +167,20 @@ def find_time_remaining_from_results(results: [str]):
     return None
 
 
-def extract_time_remaining_from_image(image: Image):
+def extract_time_remaining_from_image(image: Image.Image):
     """
     Given a PIL Image object,
     returns either a valid formatted time-remaining str (ie '11:30')
     or None.
     """
+    rgb_img = image.convert("RGB")
     results = extract_text_with_paddle(
-        image)
+        rgb_img)
     time_remaining = find_time_remaining_from_results(results)
     return time_remaining
 
 
-def extract_text_with_paddle(image) -> [str]:
+def extract_text_with_paddle(image: Image.Image) -> List[str]:
     """
     Returns a [str] containing all words found in a
     provided PIL image.
@@ -173,16 +188,16 @@ def extract_text_with_paddle(image) -> [str]:
 
     if image is None:
         return []
-    if not MODELS.models_loaded:
-        MODELS.load()
-    ideal_height = 75
+    ideal_height = 100
     scale_factor = ideal_height / image.height
     new_size = (int(image.width * scale_factor),
                 int(image.height * scale_factor))
-    image = np.array(image.resize(new_size))
-    cv2.imwrite("preprocessed_img.png", image)
+    image = image.resize(new_size)
+    img_arr = np.array(image)
+
+    # cv2.imwrite("preprocessed_img.png", img_arr)
     results = []
-    raw_result = MODELS.paddle_ocr(image)
+    raw_result = Models.paddle_ocr(img_arr)
     text_arr = raw_result[1]
     for pred in text_arr:
         word = pred[0]
@@ -190,7 +205,7 @@ def extract_text_with_paddle(image) -> [str]:
     return results
 
 
-def convert_time_to_float(time_remaining: str):
+def convert_time_to_float(time_remaining):
     """
     Coverts valid time-remaining str
     to float value representation.
--- viz.py
+++ viz.py
@@ -12,10 +12,8 @@ from PIL import ImageFont
 def visualize_timestamps(video_path, timestamps_path, viz_path, tr_roi=None):
 
     print(f"Generating visualization for video at: {video_path}")
-
     with open(timestamps_path, 'r') as f:
         timestamps = json.load(f)
-
     reader = cv2.VideoCapture(video_path)
     frame_cnt = int(reader.get(cv2.CAP_PROP_FRAME_COUNT))
     height, width, fps = int(reader.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(reader.get(
@@ -24,27 +22,27 @@ def visualize_timestamps(video_path, timestamps_path, viz_path, tr_roi=None):
 
     writer = cv2.VideoWriter(
         viz_path, cv2.VideoWriter_fourcc(*'MPEG'), fps, (width, height))
-    font = ImageFont.truetype(r'utilities\os-eb.ttf', 30) \
-
+    font = ImageFont.truetype('utilities/os-eb.ttf', 30)
     for frame_index in tqdm(range(frame_cnt)):
         ret, frame = reader.read()
         if not ret:
             break
 
         quarter, time_remaining = None, None
-        minutes, seconds = None, None
+        minutes, seconds, decimal_seconds = -1, -1, -1
         index = str(frame_index)
         if index in timestamps:
             quarter = timestamps[str(index)]["quarter"]
             time_remaining = timestamps[str(index)]["time_remaining"]
             if time_remaining is not None:
-                minutes = time_remaining // 60
-                seconds = round(time_remaining % 60, 2)
-
+                minutes = int(time_remaining) // 60
+                seconds = int((time_remaining - (minutes * 60)))
+                decimal_seconds = int(
+                    time_remaining - (minutes * 60) - seconds) * 10
         img = Image.fromarray(frame)
         draw: ImageDraw = ImageDraw.Draw(img)
         draw.text(
-            (10, 10), f"Quarter: {quarter} \nTime Remaining: {minutes}:{seconds}", font=font, fill=(255, 255, 255))
+            (10, 10), text=f"Q: {quarter} T: {minutes:02d}:{seconds:02d}.{decimal_seconds}", font=font, fill=(255, 255, 255))
         writer.write(np.array(img))
 
     writer.release()
