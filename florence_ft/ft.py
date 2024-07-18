e42538121d407e8e6ca99731bf61cb3a65655cf4
leharris3 <levlevi@email.unc.edu>
2024-07-18 14:22:34 -0400

    a working slurm ft config

--- florence_ft/ft.py
+++ florence_ft/ft.py
@@ -1,4 +1,5 @@
 # stolen from: https://github.com/andimarafioti/florence2-finetuning
+# roboflow tutorial: https://blog.roboflow.com/fine-tune-florence-2-object-detection/
 
 import yaml
 import argparse
@@ -10,6 +11,7 @@ import torch.multiprocessing as mp
 import json
 import wandb
 import warnings
+import numpy as np
 
 from PIL import Image
 from tqdm import tqdm
@@ -21,6 +23,8 @@ from torch.utils.data.distributed import DistributedSampler
 from transformers import AdamW, AutoModelForCausalLM, AutoProcessor, get_scheduler
 from transformers import logging as transformers_logging
 
+CHECKPOINT = "microsoft/Florence-2-base-ft"
+REVISION = 'refs/pr/6'
 
 wandb.login(key="3d8c09b359c1abc995fd03c27398c41afce857c1")
 warnings.filterwarnings(
@@ -36,8 +40,13 @@ transformers_logging.set_verbosity_error()
 
 class NBAClockDataset(Dataset):
 
-    def __init__(self, config):
-        annotations_fp = config["annotations_fp"]
+    def __init__(self, config, split):
+        if split == 'train':
+            annotations_fp = config["train_annotations_fp"]
+        elif split == 'val':
+            annotations_fp = config["val_annotations_fp"]
+        else:
+            raise Exception(f"Error: invalid split: {split}")
         with open(annotations_fp, "r") as f:
             self.annotations = json.load(f)
         self.image_paths = list(self.annotations.keys())
@@ -53,7 +62,7 @@ class NBAClockDataset(Dataset):
         time_on_clock = annotation.get("time_on_clock")
         if time_on_clock is not None:
             try:
-                ground_truth_text = self.format_nba_clock_time(float(time_on_clock))
+                ground_truth_text = self.format_nba_clock_time(time_on_clock)
             except ValueError as e:
                 print(f"Error formatting time: {e}")
                 ground_truth_text = "ERROR"
@@ -73,8 +82,12 @@ class NBAClockDataset(Dataset):
         Returns:
         str: Formatted time string (MM:SS, M:SS, SS.DS, or S.DS)
         """
+        # already in correct format
+        if type(seconds) == str:
+            return seconds
         if seconds is None:
             return "N/A"
+        seconds = float(seconds)
         if seconds < 0:
             raise ValueError("Time cannot be negative")
         minutes, seconds = divmod(seconds, 60)
@@ -105,13 +118,14 @@ def collate_fn(batch, processor, device):
         return_tensors="pt",
         padding=True,
         truncation=True,
-        max_length=800,
+        # max_length=800,
     ).to(device)
     return inputs, answers
 
 
 def create_data_loaders(
     train_dataset,
+    val_dataset,
     batch_size,
     num_workers,
     rank,
@@ -129,7 +143,17 @@ def create_data_loaders(
         num_workers=num_workers,
         sampler=train_sampler,
     )
-    return train_loader
+    val_sampler = DistributedSampler(
+        val_dataset, num_replicas=world_size, rank=rank
+    )
+    val_loader = DataLoader(
+        val_dataset,
+        batch_size=batch_size,
+        collate_fn=partial(collate_fn, processor=processor, device=device),
+        num_workers=num_workers,
+        sampler=val_sampler,
+    )
+    return train_loader, val_loader
 
 
 def train_model(
@@ -161,21 +185,24 @@ def train_model(
         )
 
     # train dataset
-    train_dataset = NBAClockDataset(config)
+    train_dataset = NBAClockDataset(config, split="train")
+    
+    # val dataset
+    val_dataset = NBAClockDataset(config, split="val")
+    
     # Load the model and processor
     model = AutoModelForCausalLM.from_pretrained(
-        "andito/Florence-2-large-ft", trust_remote_code=True
-    ).to(device)
+        CHECKPOINT, trust_remote_code=True, revision=REVISION).to(device)
     processor = AutoProcessor.from_pretrained(
-        "andito/Florence-2-large-ft", trust_remote_code=True
-    )
+        CHECKPOINT, trust_remote_code=True, revision=REVISION)
 
     model = DDP(model, device_ids=[rank])
 
     # Create DataLoaders
     num_workers = 0
-    train_loader = create_data_loaders(
+    train_loader, val_loader = create_data_loaders(
         train_dataset,
+        val_dataset,
         batch_size,
         num_workers,
         rank,
@@ -231,6 +258,36 @@ def train_model(
         if rank == 0:
             wandb.log({"epoch": epoch + 1, "epoch_train_loss": avg_train_loss})
 
+        # Validation phase
+        model.eval()
+        val_loss = 0
+        with torch.no_grad():
+            for batch in tqdm(
+                val_loader, desc=f"Validation Epoch {epoch + 1}/{epochs}"
+            ):
+                inputs, answers = batch
+
+                input_ids = inputs["input_ids"]
+                pixel_values = inputs["pixel_values"]
+                labels = processor.tokenizer(
+                    text=answers,
+                    return_tensors="pt",
+                    padding=True,
+                    return_token_type_ids=False,
+                ).input_ids.to(device)
+
+                outputs = model(
+                    input_ids=input_ids, pixel_values=pixel_values, labels=labels
+                )
+                loss = outputs.loss
+
+                val_loss += loss.item()
+                
+        # Log training loss to wandb
+        avg_eval_loss = val_loss / len(val_loader)
+        if rank == 0:
+            wandb.log({"epoch": epoch + 1, "epoch_train_loss": avg_eval_loss})
+                
         # Save model checkpoint
         if rank == 0:  # Only the main process saves the checkpoint
             output_dir = f"./model_checkpoints/{run_name}/epoch_{epoch + 1}"
