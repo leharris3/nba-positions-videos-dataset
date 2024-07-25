f5ae60341a9b7d528801296ccf0c32045a39ea47
leharris3 <levlevi@email.unc.edu>
2024-07-25 14:42:23 -0400

    mo stuff

--- 2d-pose-extraction/pipeline.py
+++ 2d-pose-extraction/pipeline.py
@@ -23,15 +23,22 @@ import cv2
 import numpy as np
 import codecs, json
 import os
+import typing
+import time
+import torch
+import concurrent
 
+from typing import List
 from glob import glob
 from huggingface_hub import hf_hub_download
+from easy_ViTPose.vit_utils.inference import pad_image
 from easy_ViTPose import VitInference
 from tqdm import tqdm
+from concurrent.futures import ProcessPoolExecutor, as_completed
 
 
-ext = ".pth"
-ext_yolo = ".pt"
+EXT = ".pth"
+EXT_YOLO = ".pt"
 MODEL_SIZE = "h"
 YOLO_SIZE = "s"
 DATASET = "wholebody"
@@ -39,12 +46,12 @@ MODEL_TYPE = "torch"
 YOLO_TYPE = "torch"
 REPO_ID = "JunkyByte/easy_ViTPose"
 FILENAME = (
-    os.path.join(MODEL_TYPE, f"{DATASET}/vitpose-" + MODEL_SIZE + f"-{DATASET}") + ext
+    os.path.join(MODEL_TYPE, f"{DATASET}/vitpose-" + MODEL_SIZE + f"-{DATASET}") + EXT
 )
-FILENAME_YOLO = "yolov8/yolov8" + YOLO_SIZE + ext_yolo
+FILENAME_YOLO = "yolov8/yolov8" + YOLO_SIZE + EXT_YOLO
 
 logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 logger = logging.getLogger(__name__)
 
@@ -64,55 +71,90 @@ def get_unique_frame_ids(annotations):
     return unique_ids
 
 
-def process_bbxs(config, model: VitInference, frame_obj, frame):
+def process_image(img, model):
+    
+    # TODO: we are gettting occasional ZeroDivisionError errors
+            # using a pretty shitty workaround atm
+    try:
+        img, _ = pad_image(img, 3 / 4)
+        img_input, org_h, org_w = model.pre_img(img)
+    except Exception as e:
+        logger.error(f"Failed to pad image with shape {img.shape}, error: {e}")
+        # use a dummy image
+        img = np.zeros((256, 192, 3), dtype=np.uint8)
+        img, _ = pad_image(img, 3 / 4)
+        img_input, org_h, org_w = model.pre_img(img)
+    return img_input, (org_h, org_w)
+
+
+def process_bbxs(config, model: VitInference, bbxs: List):
     """
     Predict the keypoints for all bounding boxes in a single frame.
     """
-    
-    updated_bbxs = []
-    bbxs = frame_obj["bbox"]
 
-    # optimize these values
-    x_pad = 15
-    y_pad = 15
+    @torch.no_grad()
+    def _batch_inference_torch(imgs: List[np.ndarray]) -> List[np.ndarray]:
 
-    # batch of cropped images to process
-    batch = np.array([])
-    for bbx in bbxs:
-        # crop frame
-        x, y, width, height = (
-            int(bbx["x"]) - x_pad,
-            int(bbx["y"]) - y_pad,
-            int(bbx["width"]) + (2 * x_pad),
-            int(bbx["height"]) + (2 * y_pad),
-        )
-        cropped_frame = frame[y : y + height, x : x + width]
-        # if any dim of bbx is 0, skip
-        if width == 0 or height == 0:
-            continue
-        batch = np.append(batch, cropped_frame)
-    
-    results = model.inference(batch)
-    print(f"Results: {results}")
-    assert False
-    
-        # try:
-        #     results = model.inference(cropped_frame)
-        # except Exception as e:
-        #     logger.error(f"Error processing frame {bbx['frame_id']}: {e}")
-        #     continue
-        # bbx["keypoints"] = results
-        # updated_bbxs.append(bbx)
+        og_dims = []
+        batch = []
         
-    frame_obj["bbox"] = updated_bbxs
-    return frame_obj
+        # using threadpool for now
+        start = time.time()
+        with concurrent.futures.ThreadPoolExecutor(max_workers=1024) as executor:
+            future_to_img = {executor.submit(process_image, img, model): img for img in imgs}
+            for future in concurrent.futures.as_completed(future_to_img):
+                img_input, dims = future.result()
+                batch.append(img_input)
+                og_dims.append(dims)
+                
+        # stack imgs in batch
+        batch = np.concatenate(batch, axis=0)
+        logger.debug(f"pre-processing took {time.time() - start} seconds")
+
+        # copy tensor -> cuda
+        batch = torch.from_numpy(batch).to(torch.device(model.device))
+        
+        # forward pass
+        # TODO: when using a compiled model, we seem to get "hung up" on previously unseen batch sizes
+        # this is not an issue on subsequent batches
+        start = time.time()
+        heatmaps = model._vit_pose(batch).detach().cpu().numpy()
+        logger.debug(f"forward pass took {time.time() - start} seconds")
+
+        # post process results
+        start = time.time()
+        post_processed_imgs = []
+        for i, (org_h, org_w) in enumerate(og_dims):
+            heatmap = heatmaps[i, :, :, :]
+            heatmap = np.expand_dims(heatmap, axis=0)
+            post_processed_imgs.append(model.postprocess(heatmap, org_w, org_h)[0])
+        logger.debug(f"post processing imgs took {time.time() - start} seconds")
+        
+        return post_processed_imgs
+
+    batches = []
+    results = []
+    
+    # TODO: a batch size of 512 only uses ~1/4 GPU memory
+    # we can fit multiple clips into mem at once
+    batch_size = 1024
+    
+    # generate batches
+    for i in range(0, len(bbxs), batch_size):
+        batches.append(bbxs[i : i + batch_size])
+        
+    for batch in tqdm(batches, desc="generating keypoints for bounding boxes"):
+        results.extend(_batch_inference_torch(batch))
+    return results
 
 
 def process_video(config, annotation_path: str, model: VitInference):
+    
+    # load annotations
     with open(annotation_path, "r") as f:
         annotations = json.load(f)
-        
-    # 1. get all unqiue frame ids
+
+    # get all unqiue frame ids
     unique_frame_ids = get_unique_frame_ids(annotations)
     root_dir = config["nba_ds_root_dir"]
     video_path = root_dir + annotations["video_path"]
@@ -122,18 +164,54 @@ def process_video(config, annotation_path: str, model: VitInference):
 
     cap = cv2.VideoCapture(video_path)
     frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    bounding_boxes = []
+    bounding_box_map = {}
+    
+    # optimize these values
+    x_pad = 15
+    y_pad = 15
 
-    # read frames
+    # 1. load all frames from a clip into memory
+    # 2. create a list of bbxs for entire clip [bbx1, bbx2, ...]
+    # 3. create a mapping {bbx_idx: [frame_id, bbx_index]}
+    # 4. process bbxs in batches of 64, get [results1, results2, ...]
+    # 5. update annotations file
+    # 6. write results
+    
+    start = time.time()
     frame_idx = 0
-    for i, frame_idx in zip(tqdm(range(frame_count)), unique_frame_ids.keys()):
+    for i, frame_idx in zip(range(frame_count), unique_frame_ids.keys()):
         ret, frame = cap.read()
         if not ret:
             break
         frame_obj = annotations["frames"][i]
-        updated_frame_obj = process_bbxs(config, model, frame_obj, frame)
-        annotations["frames"][i] = updated_frame_obj
-        frame_idx += 1
-
+        for bbx_frame_obj_idx, bbx in enumerate(frame_obj["bbox"]):
+            # crop frame
+            x, y, width, height = (
+                min(int(bbx["x"]) - x_pad, 0),
+                min(int(bbx["y"]) - y_pad, 0),
+                int(bbx["width"]) + (2 * x_pad),
+                int(bbx["height"]) + (2 * y_pad),
+            )
+            
+            # occasionally we run into imgs of size 0
+            # a simple but non-cost efficient fix:
+            width += 1
+            height += 1
+                
+            cropped_frame = frame[y : y + height, x : x + width]
+            bounding_boxes.append(cropped_frame)
+            bbx_idx = len(bounding_boxes) - 1
+            bounding_box_map[bbx_idx] = (i, bbx_frame_obj_idx)
+            
+    end = time.time()
+    logger.debug(f"loading frames took {end - start} seconds")
+        
+    results = process_bbxs(config, model, bounding_boxes)
+    for idx, result in enumerate(results):
+        frame_idx, bbx_frame_obj_idx = bounding_box_map[idx]
+        annotations["frames"][frame_idx]["bbox"][bbx_frame_obj_idx]["keypoints"] = result
+        
     # write results
     out_fp = (
         config["results_dir"]
@@ -152,7 +230,7 @@ def process_video(config, annotation_path: str, model: VitInference):
 def main(config):
     # TODO: distribute across gpus
     # q: how much memory are we currently using with bs = 1?
-    
+
     model_path = hf_hub_download(repo_id=REPO_ID, filename=FILENAME)
     yolo_path = hf_hub_download(repo_id=REPO_ID, filename=FILENAME_YOLO)
     logger.info(f"loading model from {model_path}")
@@ -168,11 +246,15 @@ def main(config):
     )
 
     annotation_file_paths = glob(config["clips_annotations_dir"] + "/*/*/*.json")
+    
+    files_processed = 0
     for annotation_file_path in annotation_file_paths:
         result = process_video(config, annotation_file_path, model)
         if not result:
             continue
-        break
+        files_processed += 1
+        if files_processed % 5 == 0:
+            break
 
 
 if __name__ == "__main__":
